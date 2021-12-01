b3745bfa7e7116e62a0590b9b7655a3fcae530e6
Anartz Mugika Ledo <mugan86@gmail.com>
2021-12-01 14:55:17 +0100

    1.- Part 1 - Finish - Introducción a React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      Greeting app created by <a href="https://github.com/mugan86">Anartz Mugika Ledo</a>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Anartz" age={35}/>
+      <Footer />
+    </div>
+  );
+};
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
