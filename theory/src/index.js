455b12cbda69a671551bb32d8dcf85e8e585a684
Anartz Mugika Ledo <mugan86@gmail.com>
2021-12-01 18:32:47 +0100

    Complex state: https://fullstackopen.com/es/part1/un_estado_mas_complejo_depurando_aplicaciones_react\#estado-complejo

--- theory/src/index.js
+++ theory/src/index.js
@@ -1,26 +1,50 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const Display = ({ counter }) =>  <div>{counter}</div>;
-
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-);
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+  // Estado del marcador izquierdo
+  /*const [left, setLeft] = useState(0)
+  // Estado del marcador derecho
+  const [right, setRight] = useState(0)*/
+
+  // Equivalente con propiedades en un objeto
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  });
+  /**
+   * está prohibido en React mutar el state directamente, 
+   * ya que puede provocar efectos secundarios inesperados. 
+   * Es inmutable el proceso que debemos de usar
+   * El cambio de estado siempre debe realizarse estableciendo 
+   * el estado en un nuevo objeto. Si las propiedades del 
+   * objeto de estado anterior no se modifican, 
+   * simplemente deben copiarse, lo que se hace copiando 
+   * esas propiedades en un nuevo objeto y estableciendo eso 
+   * como el nuevo estado.
+   */
+
+  // Gestionar los clicks dde la izquierda
+  const handleLeftClick = () => {
+    setClicks({ ...clicks, left: clicks.left + 1 })
+  }
+
+  // Gestionar los clicks dde la derecha
+  const handleRightClick = () => {
+    setClicks({ ...clicks, right: clicks.right + 1 })
+  }
+
   return (
     <div>
-      <Display counter={counter} />
-      <Button handleClick={increaseByOne} text="plus" />
-      <Button handleClick={decreaseByOne} text="minus" />
-      <Button handleClick={setToZero} text="zero" />
+      {clicks.left}&nbsp;&nbsp;
+      <button onClick={handleLeftClick}>
+        left
+      </button>
+      <button onClick={handleRightClick}>
+        right
+      </button>
+      &nbsp;&nbsp;{clicks.right}
     </div>
-  );
-};
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
