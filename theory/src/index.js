51e858f580350a79bf2c27591454df33e1830f92
Anartz Mugika Ledo <mugan86@gmail.com>
2021-12-02 18:52:52 +0100

    Add conditional render

--- theory/src/index.js
+++ theory/src/index.js
@@ -1,50 +1,69 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  // Estado del marcador izquierdo
-  /*const [left, setLeft] = useState(0)
-  // Estado del marcador derecho
-  const [right, setRight] = useState(0)*/
-
-  // Equivalente con propiedades en un objeto
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  });
-  /**
-   * está prohibido en React mutar el state directamente, 
-   * ya que puede provocar efectos secundarios inesperados. 
-   * Es inmutable el proceso que debemos de usar
-   * El cambio de estado siempre debe realizarse estableciendo 
-   * el estado en un nuevo objeto. Si las propiedades del 
-   * objeto de estado anterior no se modifican, 
-   * simplemente deben copiarse, lo que se hace copiando 
-   * esas propiedades en un nuevo objeto y estableciendo eso 
-   * como el nuevo estado.
-   */
-
-  // Gestionar los clicks dde la izquierda
-  const handleLeftClick = () => {
-    setClicks({ ...clicks, left: clicks.left + 1 })
-  }
-
-  // Gestionar los clicks dde la derecha
-  const handleRightClick = () => {
-    setClicks({ ...clicks, right: clicks.right + 1 })
+const History = ({allClicks}) => {
+  if (allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
   }
 
   return (
     <div>
-      {clicks.left}&nbsp;&nbsp;
-      <button onClick={handleLeftClick}>
-        left
-      </button>
-      <button onClick={handleRightClick}>
-        right
-      </button>
-      &nbsp;&nbsp;{clicks.right}
+      button press history: {allClicks.join(' ')}
     </div>
   )
 }
+
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}>{text}</button>
+)
+
+
+const App = () => {
+  // Los estados useState NUNCA DESDE un ciclo, 
+  // condicional o función que defina un componente
+  // Estado del marcador izquierdo
+  // LO QUE NO HAY QUE HACER CON EJEMPLOS:
+  // https://fullstackopen.com/es/part1/un_estado_mas_complejo_depurando_aplicaciones_react#reglas-de-los-hooks
+  const [left, setLeft] = useState(0);
+  // Estado del marcador derecho
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+  /**
+   * está prohibido en React mutar el state directamente,
+   * ya que puede provocar efectos secundarios inesperados.
+   * Es inmutable el proceso que debemos de usar
+   * El cambio de estado siempre debe realizarse estableciendo
+   * el estado en un nuevo objeto. Si las propiedades del
+   * objeto de estado anterior no se modifican,
+   * simplemente deben copiarse, lo que se hace copiando
+   * esas propiedades en un nuevo objeto y estableciendo eso
+   * como el nuevo estado.
+   */
+
+
+  return (
+    <div>
+      {left}&nbsp;&nbsp;
+      <Button onClick={handleLeftClick} text="Left" />
+      <Button onClick={handleRightClick} text="Right"/>
+      &nbsp;&nbsp;{right}
+      <History allClicks={allClicks} />
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
