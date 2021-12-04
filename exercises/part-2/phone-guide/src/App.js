518fa3291d69b05cd82536660e9c66963e0390aa
Anartz Mugika Ledo <mugan86@gmail.com>
2021-12-04 19:20:08 +0100

    Part 2 - Exercises 2.6 - 2.10 - AApply filter to show only search people

--- exercises/part-2/phone-guide/src/App.js
+++ exercises/part-2/phone-guide/src/App.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 const Person = ({ person }) => (
   <li>
-    {person.name} - Phone: {person.phone}{" "}
+    {person.name} - Phone: {person.number}{" "}
   </li>
 );
 const App = () => {
   const [persons, setPersons] = useState([
-    { id: 1, name: "Arto Hellas", phone: "292029222" },
+    { id: 1, name: "Arto Hellas", number: "040-123456" },
+    { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
+    { id: 3, name: "Dan Abramov", number: "12-43-234345" },
+    { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newPhone, setPhone] = useState("");
+  const [newSearch, setSearch] = useState("");
   // Aquí almacenamos los cambios
   const handleNameChange = (event) => setNewName(event.target.value);
   const handlePhoneChange = (event) => setPhone(event.target.value);
@@ -32,9 +36,26 @@ const App = () => {
     setPersons(persons.concat(personObject));
     resetFormValues();
   };
+
+  const filterPeople = (event) => {
+    setSearch(event.target.value);
+  };
+
+  // Filtramos en base a la búsqueda
+  const personsToShow =
+    newSearch === ""
+      ? persons
+      : persons.filter((person) => {
+          return (
+            person.name.toLowerCase().indexOf(newSearch.toLowerCase()) > -1
+          );
+        });
+
   return (
     <div>
       <h2>Phonebook</h2>
+      Name: <input value={newSearch} onChange={filterPeople} />
+      <h2>Add New contact</h2>
       <form onSubmit={addPerson}>
         <div>
           Name: <input value={newName} onChange={handleNameChange} />
@@ -48,7 +69,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
+        {personsToShow.map((person) => (
           <Person key={person.id} person={person} />
         ))}
       </ul>
