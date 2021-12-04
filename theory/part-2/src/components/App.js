7808b87fb23d0e3173619faf2138df9d71a93623
Anartz Mugika Ledo <mugan86@gmail.com>
2021-12-04 18:11:25 +0100

    Part 2- Forms basic first step to send info to proccess

--- theory/part-2/src/components/App.js
+++ theory/part-2/src/components/App.js
@@ -1,18 +1,43 @@
+import React, { useState } from "react";
 import Note from "./Note";
 
-const App = ({ notes }) => {
-    return (
-      <div>
-        <h1>Notes</h1>
-        <ul>
-          <ul>
-            {notes.map((note) => (
-              <Note key={note.id} note={note} />
-            ))}
-          </ul>
-        </ul>
-      </div>
-    );
-  };
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+  // Aquí enviamos los datos del formulario, en este caso
+  // solo tenemos una nota
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+  
+    setNotes(notes.concat(noteObject))
+    setNewNote('') // Resetear la nota
+  }
+  // Aquí almacenamos los cambios
+  const handleNoteChange = (event) => {
+    console.log(event.target.value, event.target)
+    setNewNote(event.target.value)
+  }
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote}
+          onChange={handleNoteChange}/>
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  );
+};
 
-  export default App;
+export default App;
