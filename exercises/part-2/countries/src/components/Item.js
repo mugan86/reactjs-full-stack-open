e9bf749c0b7fd187c357c77f3d09cad2a67f5941
Anartz Mugika Ledo <mugan86@gmail.com>
2021-12-05 20:29:41 +0100

    Part 2 Exercise 2.14 - Pending to finish

--- exercises/part-2/countries/src/components/Item.js
+++ exercises/part-2/countries/src/components/Item.js
@@ -1,21 +1,28 @@
+import Weather from "./Weather";
 const Item = ({ country }) => {
-  return (<div>
-    <h2>{country.name.common}</h2>
-    <p>Capital: {country.capital}</p>
-    <p>Population: {country.population}</p>
-    <h3>Languages</h3>
-    <ul>
-      {Object.keys(country.languages).map((key) => (
-        <li key={key}>{country.languages[key]}</li>
-      ))}
-    </ul>
-    <img
-      width="20%"
-      height="20%"
-      src={country.flags.png}
-      alt={country.name.common}
-    />
-  </div>)
+  return (
+    <div>
+      <h2>{country.name.common}</h2>
+      <p>Capital: {country.capital}</p>
+      <p>Population: {country.population}</p>
+      <h3>Languages</h3>
+      <ul>
+        {Object.keys(country.languages).map((key) => (
+          <li key={key}>{country.languages[key]}</li>
+        ))}
+      </ul>
+      <img
+        width="20%"
+        height="20%"
+        src={country.flags.png}
+        alt={country.name.common}
+      />
+      <Weather
+        capital={country.capital}
+        location={country.capitalInfo.latlng}
+      />
+    </div>
+  );
 };
 
 export default Item;
